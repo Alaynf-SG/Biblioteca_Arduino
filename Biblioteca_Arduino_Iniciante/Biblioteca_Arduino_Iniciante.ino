1172b78773e9d13e75b03065ab545be60016f862
Alaynf-SG <58094953+Alaynf-SG@users.noreply.github.com>
2019-11-22 22:03:06 -0300

    Update Biblioteca_Arduino_Iniciante.ino

--- Biblioteca_Arduino_Iniciante/Biblioteca_Arduino_Iniciante.ino
+++ Biblioteca_Arduino_Iniciante/Biblioteca_Arduino_Iniciante.ino
@@ -1,1 +1,92 @@
+/*******************************************************
+ *                                                     *
+ *    BIBLIOTECA PARA FINS EDUCACIONAIS                *
+ *                                                     *
+ *    DESENVOLVIDA POR ALLYSON HANSEL                  *
+ *    EQUIPE DE ROBÓTICA - SG JOÃO PESSOA              *
+ *                                                     *   
+ *    @allysonhansel                                   * 
+ *    https://github.com/Alaynf-SG/Biblioteca_Arduino  * 
+ *                                                     *
+ *                                                     *
+ *                                                     *
+ *******************************************************/
 
+
+
+
+/*******************************************************/
+
+#define pinLed1 desconectado
+#define pinLed2 desconectado
+#define pinLed3 desconectado
+
+Leds leds;
+
+/*******************************************************/
+
+#define pinoTrigger desconectado
+#define pinoEcho desconectado
+
+Ultrassonico ultrasson;
+
+/*******************************************************/
+
+#define pinServo1 desconectado
+#define pinServo2 desconectado
+
+Servos servos;
+
+/*******************************************************/
+
+#define pinBotao1 desconectado
+#define pinBotao2 desconectado
+
+Botoes botao;
+
+/*******************************************************/
+
+#define pinBuzzer1 desconectado
+#define pinBuzzer2 desconectado
+
+Buzzers buzzer;
+
+/*******************************************************/
+
+
+
+
+
+void setup(){
+/*******************************************************/
+*                                                      *
+*  leds.ligarLeds(pinLed1,pinLed2,pinLed3);            *
+*                                                      *
+/*******************************************************/
+*                                                      *
+*  ultrasson.ligarUltrassonico(pinoTrigger,pinoEcho);  *
+*                                                      *
+/*******************************************************/
+*                                                      *
+*  servos.ligarServos(pinServo1,pinServo2);            *
+*                                                      *
+/*******************************************************/
+*                                                      *
+*  botao.ligarBotoes(pinBotao1,pinBotao2);             *
+*                                                      *
+/*******************************************************/
+*                                                      *
+*  buzzer.ligarBuzzer(pinBuzzer1,pinBuzzer2);          *
+*                                                      *
+/*******************************************************/  
+}
+
+
+
+
+
+
+
+void loop(){
+  //Insira aqui a função que você deseja utilizar
+}
